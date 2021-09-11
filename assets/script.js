5cd24988b974d620d959bf28f0c5f089fd388cd0
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-11 17:52:10 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,64 @@
-$(document).ready(function() { // pulls html & css 
-    // shows current date and time 
-    var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a'); // format pulled from moment js.
-    $("#currentDay").html(todayDate); // format pulled from moment js.
+$(document).ready(function () { //loads html then css
+  //shows current date and time
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // pulled format from moment.js 
 
+  //saveBtn click listener for user enter time
+  $(".saveBtn").on("click", function () {
+      //get nearby values.
+      console.log(this);
+      var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+      var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-    // responsive save button 
-    $(".saveBtn").on("click", function() {
-      var text = $(this).siblings(".description").val(); // declares text 
-      var time = $(this).parent().attr("id"); // declares time 
-
-      //local storage 
+      //sets to local storage 
       localStorage.setItem(time, text);
-    });
+  })
+  //local storage to each hour block
+  $("#8am-hour .description").val(localStorage.getItem("8am-hour"));
 
-    //Need to do:
-    // color change for past, present, future 
-    // local storage not functioning. 
-  
-    // save date to local storage for each hour 
-    $("#8am-hour .description").val(localStorage.getItem("8am-hour"));
+  $("#9am-hour .description").val(localStorage.getItem("9am-hour"));
 
-    $("#9am-hour .description").val(localStorage.getItem("9am-hour"));
+  $("#10am-hour .description").val(localStorage.getItem("10am-hour"));
 
-    $("#10am-hour .description").val(localStorage.getItem("10am-hour"));
+  $("#11am-hour .description").val(localStorage.getItem("11am-hour"));
 
-    $("#11am-hour .description").val(localStorage.getItem("11am-hour"));
+  $("#12pm-hour .description").val(localStorage.getItem("12pm-hour"));
 
-    $("#12pm-hour .description").val(localStorage.getItem("12pm-hour"));
+  $("#1pm-hour .description").val(localStorage.getItem("1pm-hour"));
 
-    $("#13pm-hour .description").val(localStorage.getItem("13pm-hour"));
+  $("#2pm-hour .description").val(localStorage.getItem("2pm-hour"));
 
-    $("#14pm-hour .description").val(localStorage.getItem("14pm-hour."));
+  $("#3pm-hour .description").val(localStorage.getItem("3pm-hour"));
 
-    $("#15pm-hour .description").val(localStorage.getItem("15pm-hour"));
+  $("#4pm-hour .description").val(localStorage.getItem("4pm-hour"));
 
-    $("#16pm-hour .description").val(localStorage.getItem("16pm-hour"));
+  $("#5pm-hour .description").val(localStorage.getItem("5pm-hour"));
 
-    $("#17pm-hour .description").val(localStorage.getItem("17pm-hour"));
-  
-  });
+  // shows current hour
+  function timeTracker() {
+      var currentHour = moment().hour(); // pulls from moment.js 
+      
+    
+      $(".time-block").each(function () {// loop through time blocks
+          var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+          console.log( blockHour, currentHour)
+
+          //goes through css/html given classes of past, present, or future.
+          if (blockHour < currentHour) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }
+          else if (blockHour === currentHour) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+          }
+      })
+  }
+  timeTracker(); //starts funcation all over 
+})
